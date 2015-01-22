400b5bb6dbe0402fa3f240c5b286398941c0b755
Duncan M. McGreggor <oubiwann@cogitat.io>
2015-01-21 20:41:10 -0600

    Moved metadata into lfe.config & deleted package.exs.
    Addresses issue #8.

--- package.exs
+++
@@ -1,8 +0,0 @@
-Expm.Package.new(name: "lcfg",
-                 description: "Management and use of lfe.config files",
-                 version: "0.0.1",
-                 keywords: ["LFE", "Lisp", "Utility", "Library",
-                            "Configuration"],
-                 maintainers: [[name: "Duncan McGreggor",
-                                email: "oubiwann@cogitat.io"]],
-                 repositories: [[github: "lfex/lcfg"]])
